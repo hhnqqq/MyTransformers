a37538c56521943d105c93ed1f6753135f718943
hhnqqq <895228612@qq.com>
2024-04-11 03:35:37 +0000

    完成测试

--- train/trainer.py
+++ train/trainer.py
@@ -58,7 +58,7 @@ class Trainer:
 
     def save_model(self, model, step, args): 
         if args.global_rank <= 0: 
-            if not self.end: 
+            if not self.end and (step+1) % args.save_interval == 0: 
                 if step % args.save_interval == 0:
                     torch.save(model.state_dict(), os.path.join(args.output_path, f'{args.experiment_name}_{step}.ckpt'))
             else: 
