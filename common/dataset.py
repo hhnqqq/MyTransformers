bd84a1d890203c6078e407d41c56ff9237ab28ec
hhnqqq <895228612@qq.com>
2024-04-11 02:38:24 +0000

    优化数据集代码

--- common/dataset.py
+++ common/dataset.py
@@ -1,57 +1,120 @@
+# @Author: haonan he
+# @modified by: zhijian jiang
 import json
 import torch
-from torch.utils.data import Dataset
-from gemma.tokenizer import Tokenizer
 from tqdm import tqdm
 from typing import Union
 
+from torch.utils.data import Dataset
+from gemma.tokenizer import Tokenizer
+from common.utils import print_rank_0
+
 class LongRopeDataset(Dataset):
-    def __init__(self, data_path, tokenizer, max_len, max_src_len, mode:str='pretrain', read_nums:Union[int, None]=None):
+    """
+    A custom PyTorch Dataset class for loading and preprocessing long-text data.
+
+    param data_path (str): Path to the data file.
+    param tokenizer (Tokenizer): Tokenizer used to preprocess the data.
+    param max_len (int): Maximum length of the sequence.
+    param max_src_len (int): Maximum length of the input sequence.
+    param mode (str, optional): Mode of the dataset, either 'pretrain' or 'sft'. Default is 'pretrain'.
+    param read_nums (Union[int, None], optional): Number of samples to read from the data file, or None to read all. Default is None.
+    param global_rank (int, optional): Global rank of the current process. Default is 0.
+    param meta_prompt (str, optional): Meta prompt to be added to the input. Default is an empty string.
+    """
+
+    def __init__(
+        self,
+        data_path: str,
+        tokenizer: Tokenizer,
+        max_len: int,
+        max_src_len: int,
+        mode: str = 'pretrain',
+        read_nums: Union[int, None] = None,
+        global_rank: int=0,
+        meta_prompt:str =''
+    ):
         self.all_data = []
+        self.mode = mode
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        self.max_src_len = max_src_len
+        self.meta_prompt = meta_prompt
+        self.train_token_count = 0 
         with open(data_path, "r", encoding="utf-8") as fh:
             if read_nums is None:
                 read_nums = sum(1 for _ in fh)
-            for i, line in tqdm(enumerate(fh), total=read_nums, desc='start loading the dataset'):
-                if i<read_nums:
-                    sample = json.loads(line.strip())
-                    meta_prompt = 'You are a helpful long context assitant: \n'
-                    input_text = meta_prompt + 'Q:' + sample["input"] + '\n'
-                    output_text = 'A:' + sample["output"]
-                    # Tokenize input and output texts.
-                    input_tokens = tokenizer.tokenize(input_text)
-                    output_tokens = tokenizer.tokenize(output_text)
+            with open(data_path, "r", encoding="utf-8") as fh:
+                with tqdm(fh, total=read_nums, desc='loading the dataset', disable=(global_rank != 0)) as tbar:
+                        for i, line in enumerate(tbar):
+                            if i < read_nums:
+                                sample = json.loads(line.strip())
+                                assert "input" in sample.keys()
+                                input_ids, output_ids = self.preprocess_sample(sample)
+                                postfix={"train_tokens":self._get_post_fix(self.train_token_count)}   
+                                tbar.set_postfix(postfix)
+                                self.all_data.append({"input_ids": torch.LongTensor(input_ids), "labels": torch.LongTensor(output_ids)})
+        print_rank_0(f'--->train_tokens:{self._get_post_fix(self.train_token_count)}', global_rank)
 
-                    # Truncate input and output if they exceed maximum lengths.
-                    if len(input_tokens) > max_src_len:
-                        input_tokens = input_tokens[:max_src_len - 3]
+    def preprocess_sample(self, sample):
+        """
+        Preprocesses a single data sample.
 
-                    if len(output_tokens) > (max_len - len(input_tokens) - 3):
-                        output_tokens = output_tokens[:(max_len - len(input_tokens) - 3)]  # Adjust if needed.
+        param sample (dict): A dictionary containing the input and output sequences.
 
-                    # Combine tokens with special tokens.
-                    tokens = input_tokens + output_tokens
-                    input_ids = tokenizer.convert_tokens_to_ids(tokens)
-                    # Pad input_ids with pad token for labels.
-                    if mode == 'sft':
-                        labels = [tokenizer.pad_id] * len(input_tokens) + input_ids[len(input_tokens):]
-                    elif mode == 'pretrain':
-                        labels = input_ids
-                    pad_len = max_len - len(input_ids)
-                    input_ids = input_ids + [tokenizer.pad_id] * pad_len
-                    labels = labels + [tokenizer.pad_id] * pad_len
+        Returns:
+        input_ids (list): The preprocessed input sequence.
+        output_ids (list): The preprocessed output sequence.
+        """
+        if self.mode == 'sft':
+            input_text = self.meta_prompt + 'Q:' + sample["input"] + '\n'
+            output_text = 'A:' + sample["output"]
+            input_ids = self.tokenizer.encode(input_text)
+            output_ids = self.tokenizer.encode(output_text)
+            self.train_token_count += len(output_ids)
+        else:
+            input = sample["input"] + sample["output"] if "output" in sample.keys() else sample["input"]
+            input_ids = self.tokenizer.encode(input)
+            self.train_token_count += len(input_ids)
+            output_ids = []
 
-                    assert len(input_ids) == len(labels)
-                    assert len(input_ids) <= max_len
+        if len(input_ids) > self.max_src_len:
+            input_ids = input_ids[:self.max_src_len]
+        if len(output_ids) > (self.max_len - len(input_ids)):
+            output_ids = output_ids[:(self.max_len - len(input_ids))]
+        input_ids = input_ids + output_ids
 
-                    self.all_data.append({"input_ids": torch.LongTensor(input_ids), "labels": torch.LongTensor(labels)})
+        if self.mode == 'sft':
+            labels = [self.tokenizer.pad_id] * len(input_ids) + output_ids[len(input_ids):]
+        elif self.mode == 'pretrain':
+            labels = input_ids
+        pad_len = self.max_len - len(input_ids)
+        input_ids = input_ids + [self.tokenizer.pad_id] * pad_len
+        labels = labels + [self.tokenizer.pad_id] * pad_len
+
+        assert len(input_ids) == len(labels)
+        assert len(input_ids) <= self.max_len
+        return input_ids, labels
+
+    def _get_post_fix(self, count):
+        if count >= 1e9:
+            postfix = f"{count / 1e9:.2f}b"
+        elif count >= 1e6:
+            postfix = f"{count / 1e6:.2f}m"
+        elif count >= 1e3:
+            postfix = f"{count / 1e3:.2f}k"
+        else:
+            postfix = count
+        return postfix
 
     def __len__(self):
         return len(self.all_data)
 
     def __getitem__(self, idx):
-        instance = self.all_data[idx]
-        return instance
-
+        return self.all_data[idx]
+    
 if __name__ == '__main__':
     # Test example.
-    pass
+    z = LongRopeDataset('/home/modelfun/zhaokangkang/mini_LLama/gemma-data/LongQLoRA-SFT-Data-2k.jsonl', 
+                        Tokenizer('/home/modelfun/zhaokangkang/mini_LLama/gemma/tokenizer.model'), 1500, 256, 'pretrain',1000,1)
+    print(z[0])
