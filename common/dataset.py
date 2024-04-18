55d2a2555f932513dd91453c79970e100eecdd34
hhnqqq <895228612@qq.com>
2024-04-18 08:12:02 +0000

    支持txt文件

--- common/dataset.py
+++ common/dataset.py
@@ -32,7 +32,9 @@ class LongRopeDataset(Dataset):
         mode: str = 'pretrain',
         read_nums: Union[int, None] = None,
         global_rank: int=0,
-        meta_prompt:str =''
+        meta_prompt:str ='',
+        prefix:str='Q:',
+        postfix:str='A:'
     ):
         self.all_data = []
         self.mode = mode
@@ -40,11 +42,14 @@ class LongRopeDataset(Dataset):
         self.max_len = max_len
         self.max_src_len = max_src_len
         self.meta_prompt = meta_prompt
+        self.prefix = prefix
+        self.postfix = postfix
         self.train_token_count = 0 
         print_rank_0(f'--->using dataset: {data_path}', global_rank)
         print_rank_0(f'--->training mode: {self.mode}', global_rank)
         print_rank_0(f'--->meta prompt: {self.meta_prompt}', global_rank)
         print_rank_0(f'--->tokenizer name: {type(self.tokenizer).__name__}', global_rank)
+        print_rank_0(f'--->using meta prompt: {meta_prompt}, prefix: {prefix}, postfix: {postfix}', global_rank)
         with open(data_path, "r", encoding="utf-8") as fh:
             if read_nums is None:
                 read_nums = sum(1 for _ in fh)
@@ -52,7 +57,10 @@ class LongRopeDataset(Dataset):
                 with tqdm(fh, total=read_nums, desc='loading the dataset', disable=(global_rank != 0)) as tbar:
                         for i, line in enumerate(tbar):
                             if i < read_nums:
-                                sample = json.loads(line.strip())
+                                try:
+                                    sample = json.loads(line.strip())
+                                except:
+                                    sample = line.strip()
                                 input_ids, output_ids = self.preprocess_sample(sample)
                                 postfix={"train_tokens":self._get_post_fix(self.train_token_count)}   
                                 tbar.set_postfix(postfix)
@@ -71,8 +79,8 @@ class LongRopeDataset(Dataset):
         """
         if self.mode == 'sft':
             # In the case of sft, the input sample mast be a instance of dict.
-            input_text = self.meta_prompt + 'Q:' + sample["input"] + '\n'
-            output_text = 'A:' + sample["output"]
+            input_text = self.meta_prompt + self.prefix + sample["input"] + '\n'
+            output_text = self.postfix + sample["output"]
             input_ids = self.tokenizer.encode(input_text)
             output_ids = self.tokenizer.encode(output_text)
             self.train_token_count += len(output_ids)
@@ -80,9 +88,10 @@ class LongRopeDataset(Dataset):
             # In the case of pretrain, the input sample can be a single string.
             if isinstance(sample, dict):
                 assert "input" in sample.keys(), "Can not find input information in the dataset"
-                input = sample["input"] + sample["output"] if "output" in sample.keys() else sample["input"]
+                input = self.meta_prompt + self.prefix + sample["input"] + self.postfix
+                input += sample["output"] if "output" in sample.keys() else ''
             elif isinstance(sample, str):
-                input = sample
+                input = self.meta_prompt + self.prefix + sample + self.postfix
             else:
                 raise ValueError("You are using a not supported file format, please use jsonl or txt.")
             input_ids = self.tokenizer.encode(input)
@@ -123,7 +132,7 @@ class LongRopeDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.all_data[idx]
-    
+        
 if __name__ == '__main__':
     # Test example.
     z = LongRopeDataset('/home/modelfun/zhaokangkang/mini_LLama/gemma-data/LongQLoRA-SFT-Data-2k.jsonl', 
