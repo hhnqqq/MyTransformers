2baf43bfdcbb9a0a8bcccab3dc1bb3b038c1844a
hhnqqq <895228612@qq.com>
2024-10-28 15:19:05 +0800

    完善vera代码（未测试）

--- common/lora_modules/vera.py
+++ common/lora_modules/vera.py
@@ -1,78 +1,104 @@
-""" Implements VeRA"""
+# @author: haonan he
+# @date: 2024-10-28
+"""Implementation of VeRA (Vector-based Random Matrix Adaptation) based on the paper:
+https://arxiv.org/abs/2310.11454.
+
+VeRA builds upon LoRA, where LoRA updates the weight matrix W by training two
+low-rank matrices, A and B, with an intermediate rank r. In VeRA, these matrices
+are frozen, shared across all layers, and adapted using trainable vectors d and b.
+This significantly reduces the number of trainable parameters.
+
+In both approaches, the low-rank matrices and vectors can be merged into the
+original weight matrix W without introducing additional inference latency. This
+means VeRA can maintain a much smaller number of trainable parameters even with
+a larger rank.
+
+Note that this implementation does not share the same weights b and a across the
+model. For the `shared across layers` implementation, please refer to:
+https://github.com/huggingface/peft/blob/main/src/peft/tuners/vera/model.py#L43
+
+For the ablation study on shared parameters, we quote from the paper:
+`Sharing Random Matrices: We conduct experiments on RTE, MRPC, CoLA, and STS-B
+tasks to assess the impact of sharing random matrices on performance. We evaluate
+two setups: one with random matrices shared across all adapted layers, and another
+with uniquely generated matrices for each layer. Results in Table 7 show that the
+mean performance is identical for RTE and STS-B tasks, and there is a slight
+improvement for MRPC and CoLA when using unique matrices.`"""
+
 import torch
-import torch.nn as nn
 
 from common.lora_modules.lora import *
     
 class LinearWithVeRA(LinearWithLoRA):
     def __init__(self,
         lora_config: LoRAConfig,
-        scaling_vector_b_init_method: str = 'zero',
-        scaling_vector_d_init_method: str = 'ones'
+        lambda_b_init_method: str = 'zero',
+        lambda_d_init_method: str = 'ones'
     ):
         """
         Initialize the LinearWithVeRA layer.
 
         Args:
-            scaling_vector_b_init_method (str, optional): Initialization method for scaling vector b. ['zero', 'ones', 'small_constant', 'random']. Default is 'zeros'.
-            scaling_vector_d_init_method (str, optional): Initialization method for scaling vector d. ['zero', 'ones', 'small_constant', 'random']. Default is 'ones'.
+            lambda_b_init_method (str, optional): Initialization method for lambda b. ['zero', 'ones', 'small_constant', 'random']. Default is 'zeros'.
+            lambda_d_init_method (str, optional): Initialization method for lambda d. ['zero', 'ones', 'small_constant', 'random']. Default is 'ones'.
         """
         super().__init__(lora_config)
-        self._init_scaling_vectors(scaling_vector_b_init_method, scaling_vector_d_init_method)
+        self._init_lambdas(lambda_b_init_method, lambda_d_init_method)
+        if self.weight_b_init_method is None:
+            raise ValueError('The init method for weight b in vera can not be zero.')
     
-    def _init_lora_weights(self):
-        dtype = torch.int8 if self.quant else None
-
-        # Initialize shared matrix a and b, and frozen them.
-        self.weight_a = nn.Parameter(torch.randn((self.lora_rank, self.in_features), dtype=dtype), requires_grad=requires_grad)
-        self.weight_b = nn.Parameter(torch.randn((self.out_features, self.lora_rank), dtype=dtype), requires_grad=requires_grad)
-
-        if self.quant:
-            self.weight_a_scaler = nn.Parameter(torch.Tensor(self.lora_rank))
-            self.weight_b_scaler = nn.Parameter(torch.Tensor(self.out_features))
-
-        self._init_weight('weight_a')
-        self._init_weight('weight_b')
-    
-    def _init_scaling_vectors(self, b_init_method: str, d_init_method: str):
+    def _init_lambdas(self, b_init_method: str, d_init_method: str):
         """
-        Initialize scaling vectors with different methods.
+        Initialize lambdas with different methods.
         Args:
-            b_init_method: The method to initialize scaling vector b.
-            d_init_method: The method to initialize scaling vector d.
+            b_init_method: The method to initialize lambda b.
+            d_init_method: The method to initialize lambda d.
         """
         # Initialize vector b
+        dtype = self._get_lora_dtype()
         if b_init_method == 'zero':
-            self.scaling_vector_b = torch.zeros(self.out_features)
+            self.lambda_b = torch.zeros(self.out_features, dtype=dtype)
         elif b_init_method == 'ones':
-            self.scaling_vector_b = torch.ones(self.out_features)
+            self.lambda_b = torch.ones(self.out_features, dtype=dtype)
         elif b_init_method == 'small_constant':
-            self.scaling_vector_b = 0.1 * torch.ones(self.out_features)
+            self.lambda_b = 0.1 * torch.ones(self.out_features, dtype=dtype)
         elif b_init_method == 'random':
-            self.scaling_vector_b = torch.rand(self.out_features)
+            self.lambda_b = torch.rand(self.out_features, dtype=dtype)
         else:
             raise ValueError(f"Unknown b_init_method: {b_init_method}")
 
         # Initialize vector d
         if d_init_method == 'zero':
-            self.scaling_vector_d = torch.zeros(self.lora_rank)
+            self.lambda_d = torch.zeros(self.lora_rank, dtype=dtype)
         elif d_init_method == 'ones':
-            self.scaling_vector_d = torch.ones(self.lora_rank)
+            self.lambda_d = torch.ones(self.lora_rank, dtype=dtype)
         elif d_init_method == 'small_constant':
-            self.scaling_vector_d = 0.1 * torch.ones(self.lora_rank)
+            self.lambda_d = 0.1 * torch.ones(self.lora_rank, dtype=dtype)
         elif d_init_method == 'random':
-            self.scaling_vector_d = torch.rand(self.lora_rank)
+            self.lambda_d = torch.rand(self.lora_rank, dtype=dtype)
         else:
             raise ValueError(f"Unknown d_init_method: {d_init_method}")
 
+    def _lora_forward(self, x: torch.Tensor, result: torch.Tensor) -> torch.Tensor:
+        # Called by forward method in LinearWithLoRA
+        weight_a = self._quantize_weight(self.weight_a, self.weight_a_quantizer)
+        weight_b = self._quantize_weight(self.weight_b, self.weight_b_quantizer)
+        lora_result = self.lambda_b * F.linear(
+            self.lambda_d * F.linear(
+                self.lora_dropout(x), 
+                weight_a
+            ), 
+            weight_b
+        ).to(result.dtype)
+
+        return result + self.lora_scaler * lora_result
+    
     def _compute_lora(self): 
+        # Called by merge lora method in LinearWithLoRA
         if self.has_lora_weights:
             # Compute adapted lora weights.
-            adapted_weight_a = torch.matmul(self.weight_a, torch.diag(self.scaling_vector_b))
-            adapted_weight_b = torch.matmul(self.weight_b, torch.diag(self.scaling_vector_d))
-            # Compute lora weights.
-            weight_a = self._quantize_weight(adapted_weight_a, self.weight_a_quantizer)
-            weight_b = self._quantize_weight(adapted_weight_b, self.weight_b_quantizer)
+            weight_a = self._quantize_weight(self.weight_a, self.weight_a_quantizer) * self.lambda_d
+            weight_b = self._quantize_weight(self.weight_b, self.weight_b_quantizer) * self.lambda_b
 
             lora_weight = self.lora_scaler * torch.matmul(weight_b, weight_a)
 
