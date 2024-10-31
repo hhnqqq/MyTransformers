87462a5ab803d47c1ba2ebe05a5faa6e567f46b7
hhnqqq <895228612@qq.com>
2024-10-31 10:31:14 +0800

    在dora的计算过程中保持精度与lora权重一致

--- common/lora_modules/dora.py
+++ common/lora_modules/dora.py
@@ -16,6 +16,9 @@ class LinearWithDoRA(LinearWithLoRA):
     def _apply_dora(self, weight: torch.Tensor) -> torch.Tensor:
         origin_magnitude = torch.norm(weight, p=2, dim=0, keepdim=True)
         lora_weight = self._compute_lora_weight()
+        # Make sure that the dtype of weight same as dtype of lora weights.
+        origin_weight_dtype = weight.dtype
+        weight = weight.to(lora_weight.dtype)
         weight = weight + lora_weight * self.lora_scaler
         new_magnitude = torch.norm(weight, p=2, dim=0, keepdim=True)
         # see section 4.3 of DoRA (https://arxiv.org/abs/2402.09353)
@@ -26,12 +29,12 @@ class LinearWithDoRA(LinearWithLoRA):
         # during backpropagation"
         new_magnitude = new_magnitude.detach()
 
-        # In peft. This should be added on top of the base layer output.:
+        # In peft. This should be added on top of the base layer output.
         # result_dora = (mag_norm_scale - 1) * (
         # F.linear(x, transpose(weight, self.fan_in_fan_out))
         # ) + mag_norm_scale * lora_result * scaling
         weight = (origin_magnitude / new_magnitude) * weight
-        return weight
+        return weight.to(origin_weight_dtype)
 
     def _merge_lora(self) -> bool:
         # Merge the lora weight into full rank weight if possible.
