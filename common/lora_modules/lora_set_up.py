621a73034b143cfda2952dd0e7d775c317fbef4f
hhnqqq <895228612@qq.com>
2024-10-31 10:33:34 +0800

    支持更多变体，修复一些传参错误

--- common/lora_modules/lora_set_up.py
+++ common/lora_modules/lora_set_up.py
@@ -1,5 +1,4 @@
 from argparse import Namespace
-from typing import Union, List
 
 from common.utils import print_rank_0
 from common.lora_modules.lora import *
@@ -9,6 +8,9 @@ from common.lora_modules.plora import LinearWithPLoRA
 from common.lora_modules.lora_ga import LinearWithLoRAGA
 from common.lora_modules.mos_lora import LinearWithMosLoRA
 from common.lora_modules.rslora import LinearWithRSLoRA
+from common.lora_modules.pissa import LinearWithPiSSA
+from common.lora_modules.olora import LinearWithOLoRA
+from common.lora_modules.vera import LinearWithVeRA
 
 def get_lora_layer_class(args):
     variant_config = {}
@@ -27,10 +29,17 @@ def get_lora_layer_class(args):
         lora_layer_class = LinearWithLoRAGA
     elif getattr(args, "use_rslora", False):
         lora_layer_class = LinearWithRSLoRA
+    elif getattr(args, "use_pissa", False):
+        lora_layer_class = LinearWithPiSSA
+        variant_config = dict(n_iters=args.pissa_n_iters)
+    elif getattr(args, "use_olora", False):
+        lora_layer_class = LinearWithOLoRA
+    elif getattr(args, 'use_vera', False):
+        lora_layer_class = LinearWithVeRA
     else:
         lora_layer_class = LinearWithLoRA
 
-    print_rank_0(f'Using lora variant: {lora_layer_class.__name__}', rank=args.global_rank)
+    print_rank_0(f'--->Using lora variant: {lora_layer_class.__name__}', rank=args.global_rank)
     return lora_layer_class, variant_config
 
 def switch_to_lora(model: nn.Module, 
@@ -41,18 +50,20 @@ def switch_to_lora(model: nn.Module,
 
     Args:
         model: Any pytorch model.
-        replace_names: List of module names to be replaced by LoRA.
+        replace_modules: List of module names to be replaced by LoRA.
         rank: Rank for LoRA.
         lora_scaler: Scaler for LoRA.
         transposition: nn.Linear(x, w) compute xw^T, so the weight should in shape [out_feature, in_feature]. Otherwise, transposition should be set to True
         use_dora: Weather to use dora
         plora_steps: The steps to merge and reset lora weight.
     """
-    assert args.replace_names is not None, 'Replace names can not be None'
+    assert args.replace_modules is not None, 'Replace names can not be None'
     lora_layer_class, variant_config = get_lora_layer_class(args)
+    if args.run_lora_in_fp32:
+        print_rank_0('--->Will keep lora weights in float32', args.global_rank)
     for name, module in model.named_modules():
         replace_tag = False
-        for replace_name in args.replace_names:
+        for replace_name in args.replace_modules:
             if replace_name in name:
                 # Create LoRA layer instance.
                 replace_tag = True
@@ -61,7 +72,7 @@ def switch_to_lora(model: nn.Module,
                 elif isinstance(module, nn.Module):
                     if  all(hasattr(module, attr) for attr in ["in_features", "out_features", "weight"]):
                         quant = getattr(module, "quant", False)
-                        lora_config = LoRAConfig(lora_rank=args.rank, 
+                        lora_config = LoRAConfig(lora_rank=args.lora_rank, 
                                             lora_scaler=args.lora_scaler, 
                                             lora_dropout=args.lora_dropout,
                                             run_lora_in_fp32=args.run_lora_in_fp32,
