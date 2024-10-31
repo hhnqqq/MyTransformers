6a6337f8c3c4fa79b550da1a709a409a74c9fb28
hhnqqq <895228612@qq.com>
2024-10-31 10:37:14 +0800

    优化代码

--- common/lora_modules/pissa.py
+++ common/lora_modules/pissa.py
@@ -5,34 +5,36 @@ class LinearWithPiSSA(LinearWithLoRA):
     def __init__(
         self,
         lora_config: LoRAConfig,
-        n_iters: Optional[int] = None
+        n_iters: Optional[int] = None,
+        fast_svd: bool = True
     ):
-        super().__init__(lora_config)
         self.n_iters = n_iters
+        self.fast_svd = fast_svd
+        super().__init__(lora_config)
 
     def _init_lora_weights(self):
-        dtype = torch.int8 if self.quant else self.weight.dtype
+        # PiSSA share same functions with vinalla lora only with a different initialize method.
+        dtype = self._get_lora_dtype()
+        weight_dtype = self.weight.dtype
         requires_grad = not self.quant
 
         weight = self.weight.to(torch.float32)
         if self.n_iters > 2:
-            Vr, Sr, Ur = svd_lowrank(
-                weight.data, self.lora_rank, niter=self.n_iters
-            )
-            Sr /= self.lora_scaler
+            # Run fast svd.
+            Vr, Sr, Ur = svd_lowrank(weight.data, self.lora_rank, niter=self.n_iters)
             Uhr = Ur.t()
         else:
+            # Full svd, which is very slow.
             V, S, Uh = torch.linalg.svd(self.weight.data, full_matrices=False)
-            Vr = V[:, : self.lora_rank]
-            Sr = S[: self.lora_rank]
-            Sr /= self.lora_scaler
-            Uhr = Uh[: self.lora_rank]
+            Vr, Sr, Uhr = V[:, :self.lora_rank], S[:self.lora_rank], Uh[:self.lora_rank]
+        Sr /= self.lora_scaler
                
-        self.weight_a = nn.Parameter(torch.diag(torch.sqrt(Sr)) @ Uhr, requires_grad=requires_grad, dtype=dtype)
-        self.weight_b = nn.Parameter(Vr @ torch.diag(torch.sqrt(Sr)), requires_grad=requires_grad, dtype=dtype)
+        sqrt_Sr = torch.sqrt(Sr)
+        self.weight_a = nn.Parameter((torch.diag(sqrt_Sr) @ Uhr).to(dtype), requires_grad=requires_grad)
+        self.weight_b = nn.Parameter((Vr @ torch.diag(sqrt_Sr)).to(dtype), requires_grad=requires_grad)
+
         if self.quant:
             self.weight_a_scaler = nn.Parameter(torch.Tensor(self.lora_rank))
             self.weight_b_scaler = nn.Parameter(torch.Tensor(self.out_features))
 
-        weight = weight.data - self._compute_lora_weight()
-        self.weight.data = weight.to(dtype)
+        self.weight.data = (weight - self._compute_lora_weight()).to(weight_dtype)
