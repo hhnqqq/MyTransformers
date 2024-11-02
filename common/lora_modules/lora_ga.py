261e185a916633f0f20b57d1aded436bc2ed2eeb
hhnqqq <895228612@qq.com>
2024-11-02 21:23:04 +0800

    优化lora-ga代码，确保累计的梯度被正确的all reduced， 减少不必要的计算

--- common/lora_modules/lora_ga.py
+++ common/lora_modules/lora_ga.py
@@ -1,6 +1,9 @@
-from collections.abc import Iterable
+"""Implementation of LoRA-GA
+Code reference: https://github.com/Outsider565/LoRA-GA/blob/main/peft/src/peft/tuners/lora/layer.py
+"""
+import random
 
-from common.utils import print_rank_0
+from common.utils import print_rank_0, reduce_tensor, Timer
 from common.utils import to_device
 from common.lora_modules.lora import *
 
@@ -9,7 +12,9 @@ class LinearWithLoRAGA(LinearWithLoRA):
                         direction: str = 'ArB2r', 
                         scale: str = 'gd', 
                         stable_gamma: int = 16, 
-                        scaling_factor: int = 16):
+                        scaling_factor: int = 16,
+                        global_rank: int = 0,
+                        is_first: bool = False):
         """
         Reinitialize the LoRA weights based on the gradient of the original weight matrix.
 
@@ -33,51 +38,60 @@ class LinearWithLoRAGA(LinearWithLoRA):
         Note: This method assumes that the LinearWithLora layer has gradient. Please call this
         method in the first step of training(before the model.step() call, or the gradient will be cleared.)
         """
+        if not hasattr(self.weight, 'grad_stored'):
+            return
+        
+        # Perform SVD on the weight gradient
+        # Weight stored gradient shape [out_feature, in_feature]
+        U, S, V = torch.svd_lowrank(self.weight.grad_stored.float().cuda(), q=4 * self.lora_rank, niter=4)
+        # U shape [out_feature, 4r] S shape [4r, 4r] V shape [in_feature, 4r]
+        V = V.T
 
-        if hasattr(self.weight, 'grad_stored'):
-            # Perform SVD on the weight gradient
-            # Weight stored gradient shape [out_feature, in_feature]
-            U, S, V = torch.svd_lowrank(self.weight.grad_stored.float().cuda(), q=4 * self.lora_rank, niter=4)
-            # U shape [out_feature, 4r] S shape [4r, 4r] V shape [in_feature, 4r]
-            V = V.T
+        # Determine A and B based on the direction parameter
+        if direction == "ArBr":
+            # B shape [out_feature, r/2]
+            B = U[:, 0:2 * self.lora_rank:2]
+            A = V[1:2 * self.lora_rank:2, :]
+        elif direction == "A2rBr":
+            # B shape [out_feature, r]
+            B = U[:, :self.lora_rank]
+            # A shape [r, in_feature]
+            A = V[self.lora_rank:2 * self.lora_rank, :]
+        elif direction == "ArB2r":
+            B = U[:, self.lora_rank:2 * self.lora_rank]
+            A = V[:self.lora_rank, :]
+        elif direction == "random":
+            random_list = random.sample(range(2 * self.lora_rank), 2 * self.lora_rank)
+            indexes_A = random_list[0:self.lora_rank]
+            indexes_B = random_list[self.lora_rank:2 * self.lora_rank]
+            B = U[:, indexes_B]
+            A = V[indexes_A, :]
+        else:
+            raise ValueError(f"Unknown direction: {direction}")
 
-            # Determine A and B based on the direction parameter
-            if direction == "ArBr":
-                # B shape [out_feature, r/2]
-                B = U[:, 0:2 * self.lora_rank:2]
-                A = V[1:2 * self.lora_rank:2, :]
-            elif direction == "A2rBr":
-                # B shape [out_feature, r]
-                B = U[:, :self.lora_rank]
-                # A shape [r, in_feature]
-                A = V[self.lora_rank:2 * self.lora_rank, :]
-            elif direction == "ArB2r":
-                B = U[:, self.lora_rank:2 * self.lora_rank]
-                A = V[:self.lora_rank, :]
-            else:
-                raise ValueError(f"Unknown direction: {direction}")
+        # Apply scaling to A and B based on the scale parameter
+        if scale == "gd":
+            A /= scaling_factor
+            B /= scaling_factor
+        elif scale == "stable":
+            m, n = self.weight.grad_stored.shape 
+            A = A * m**0.25 / stable_gamma**0.5
+            B = B * m**0.25 / stable_gamma**0.5
+        elif scale == "weightS":
+            _, S, _ = torch.svd_lowrank(self.weight.float(), q=4 * self.lora_rank, niter=4)
+            S /= scaling_factor
+            avg_s = torch.sqrt(S[:self.lora_rank]).mean().to(A.device)
+            A *= avg_s
+            B *= avg_s
+        elif scale != "unit":
+            raise ValueError(f"Unknown scale: {scale}")
+        del self.weight.grad_stored
 
-            # Apply scaling to A and B based on the scale parameter
-            if scale == "gd":
-                A /= scaling_factor
-                B /= scaling_factor
-            elif scale == "stable":
-                m, n = self.weight.grad_stored.shape 
-                A = A * m**0.25 / stable_gamma**0.5
-                B = B * m**0.25 / stable_gamma**0.5
-            elif scale == "weightS":
-                _, S, _ = torch.svd_lowrank(self.weight.float(), q=4 * self.lora_rank, niter=4)
-                S /= scaling_factor
-                avg_s = torch.sqrt(S[:self.lora_rank]).mean().to(A.device)
-                A *= avg_s
-                B *= avg_s
-            elif scale != "unit":
-                raise ValueError(f"Unknown scale: {scale}")
-            del self.weight.grad_stored
-
-            # Update the LoRA weights
-            self.weight_a.data = A.contiguous().cuda()
-            self.weight_b.data = B.contiguous().cuda()
+        # Update the LoRA weights
+        self.weight_a.data = A.contiguous().cuda()
+        self.weight_b.data = B.contiguous().cuda()
+        if is_first:
+            print_rank_0(f'--->LoRA-GA re-init example: weight_B->{self.weight_b}', global_rank)
 
 def get_record_gradient_hook(model):
     def record_gradient_hook(grad):
@@ -128,32 +142,44 @@ def lora_ga_reinit(
     Note: This implementation follows the LoRA-GA paper's algorithm closely.
     """
     print_rank_0("--->Estimating gradient for lora ga.", rank=args.global_rank)
-    model.to(args.device)
-    model.train()
-    hooks = []
-    for param in model.parameters():
-        param.requires_grad = True
-        hook = param.register_hook(get_record_gradient_hook(model))
-        hooks.append(hook)
-    for idx, batch in enumerate(dataloader):
-        batch = to_device(batch, args.device)
-        output = model(**batch)
-        if args.huggingface:
-            output.loss.backward()
-        else:
-            output[0].backward()
-        get_record_gradient_hook(model)(None)
-        for p in model.parameters():
-            if p.grad is not None:
+    with Timer() as timer:
+        model.to(args.device)
+        model.train()
+
+        hooks = [
+            module.weight.register_hook(get_record_gradient_hook(model))
+            for module in model.modules()
+            if isinstance(module, LinearWithLoRA)
+        ]
+
+        for idx, batch in enumerate(dataloader):
+            batch = to_device(batch, args.device)
+            output = model(**batch)
+            loss = output.loss if args.huggingface else output[0]
+            loss.backward()
+
+            for p in model.parameters():
                 p.grad = None
-        if (idx+1) == iters:
-            break
-    for p in model.parameters():
-        p.grad_stored /= iters
-    for hook in hooks:
-        hook.remove()
-    torch.cuda.empty_cache()
-    for name, module in model.named_modules():
-        if isinstance(module, LinearWithLoRAGA):
-            print_rank_0(f'--->Module {name} is reinitiating lora weight', args.global_rank)
-            module.gradient_reinit()
+
+            if (idx + 1) == iters:
+                break
+
+        for hook in hooks:
+            hook.remove()
+
+        torch.cuda.empty_cache()
+        torch.distributed.barrier()
+
+        print_rank_0('--->All reduce LoRA-GA stored gradients if needed.', args.global_rank)
+        for p in model.parameters():
+            if hasattr(p, 'grad_stored'):
+                p.grad_stored /= iters
+                if args.world_size > 1:
+                    p.grad_stored = reduce_tensor(p.grad_stored.to(args.device), args.world_size).to('cpu')
+
+        for if_first, (name, module) in enumerate(model.named_modules()):
+            if isinstance(module, LinearWithLoRAGA):
+                print_rank_0(f'--->Module {name} is reinitiating lora weight', args.global_rank)
+                module.gradient_reinit(scale=args.lora_ga_scale_method, global_rank=args.global_rank, is_first=(if_first == 0))
+
+    print_rank_0(f'--->Total time consumed for LoRA-GA initialization: {timer.time_cost}', args.global_rank)
