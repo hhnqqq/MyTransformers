2a7fdb43c3bc1a01238942624999362abb9c5274
hhnqqq <895228612@qq.com>
2024-11-07 15:15:17 +0800

    优化lorapro代码（仍然无法收敛，但是参数能够正常更新）

--- common/lora_modules/lorapro_optim.py
+++ common/lora_modules/lorapro_optim.py
@@ -11,7 +11,7 @@ from torch.optim import Optimizer
 from torch._utils import is_compiling
 from scipy.linalg import solve_sylvester
 
-from common.utils import print_rank_0, Timer
+from common.utils import print_rank_0
 
 def find_lora_names(n):
     for substring in ['weight_a', 'weight_b']:
@@ -52,10 +52,10 @@ def solve_sylvester(A, B, C, X=None):
     n = A.shape[-1];
     R, U = torch.linalg.eig(A)
     S, V = torch.linalg.eig(B)
-    F = torch.linalg.solve(U, (C + 0j) @ V)
+    F = torch.linalg.solve(U, torch.matmul((C + 0j), V))
     W = R[..., :, None] - S[..., None, :]
     Y = F / W
-    X = U[...,:n,:n] @ Y[...,:n,:m] @ torch.linalg.inv(V)[...,:m,:m]
+    X = torch.matmul(torch.matmul(U[...,:n,:n], Y[...,:n,:m]), torch.linalg.inv(V)[...,:m,:m])
     return X.real if all(torch.isreal(x.flatten()[0]) 
                 for x in [A, B, C]) else X
 
@@ -64,14 +64,14 @@ class LoRAProAdamW(Optimizer):
         self,
         named_params: List[Tuple[str, torch.Tensor]],
         lr: Union[float, torch.Tensor] = 1e-3,
-        scaling_factor: float = 2.,
+        lora_scaler: float = 2.,
         betas: Tuple[float, float] = (0.9, 0.999),
         eps: float = 1e-8,
         weight_decay: float = 0,
         amsgrad: bool = False,
         maximize: bool = False,
         differentiable: bool = False,
-        X_mode: str = "zero",
+        X_mode: str = "sylvester",
         lora_plus_scaler: int = 1
     ):
         
@@ -96,38 +96,33 @@ class LoRAProAdamW(Optimizer):
         self.X_mode = X_mode
         self.step_ = 0
         self.lora_plus_scaler = lora_plus_scaler
+        self.named_param_dtype = {}
         
         if not isinstance(named_params, list):
             named_params = [named_params]
         # Process named_params into param groups
         params = []
-        name_shape_mapping = {}
 
         for named_params_group in named_params:
             param_group = {
                 'params': [],
+                'params_fp32': [],
                 'names': [],
+                'grads': [],
                 'lr': named_params_group.get('lr', lr),
-                'scaling_factor': scaling_factor,
-                'betas': betas,
-                'eps': eps,
-                'weight_decay': weight_decay,
-                'amsgrad': amsgrad,
-                'maximize': maximize,
-                'differentiable': differentiable,
-                'X_mode': X_mode
             }
 
             for name, param in named_params_group['params']:
                 param_group['params'].append(param)
+                param_group['params_fp32'].append(param.detach().clone().float())
                 param_group['names'].append(name)
-                name_shape_mapping[name] = param.shape
+                self.named_param_dtype[name] = param.dtype
                 
             params.append(param_group)
         
         defaults = dict(
             lr=lr,
-            scaling_factor=scaling_factor,
+            lora_scaler=lora_scaler,
             betas=betas,
             eps=eps,
             weight_decay=weight_decay,
@@ -154,44 +149,56 @@ class LoRAProAdamW(Optimizer):
                 
         for group in self.param_groups:
             self._update_group_params(group)
+            for name, param_fp32, param in zip(group["names"], group["params_fp32"], group["params"]):
+                param.data.copy_(param_fp32.data)
 
         return loss
 
     def _update_group_params(self, group):
         beta1, beta2 = group["betas"]
-        scaling_factor = group["scaling_factor"]
+        lora_scaler = group["lora_scaler"]
 
-        param_dict = {}
-        for p, n in zip(group["params"], group["names"]):
-            if p.grad is None:
+        param_dict, grad_dict = {}, {}
+        for i in range(len(group['params'])):
+            # Ensure p.data.dtype is torch.float32
+            param = group['params'][i]
+            param_fp32 = group['params_fp32'][i]
+            grad = param.grad
+            name = group['names'][i]
+            if grad is None:
                 continue
-            lora_weight_name = find_lora_names(n)
+            lora_weight_name = find_lora_names(name)
             if lora_weight_name:
-                param_dict[lora_weight_name] = p
-                if len(param_dict.keys()) == 2:
-                    name = n[: n.find(lora_weight_name)] + 'lora'
-                elif len(param_dict.keys()) == 1:
+                base_name = name[: name.find(lora_weight_name)]
+                param_dict[lora_weight_name] = param_fp32
+                grad_dict[lora_weight_name] = grad
+                if len(param_dict.keys()) == 1:
                     continue
+                elif len(param_dict.keys()) == 2:
+                    name = base_name + 'lora'
             else:
-                name = n
+                name = name
             
             state = self.state[name]
         
             if len(state) == 0:
-                self._initialize_state(state, param_dict, p, group)
+                self._initialize_state(state, param_dict, param_fp32, group)
 
-        if len(param_dict.keys()) == 2:
-            self._update_lora_params(state, param_dict, group, scaling_factor)
-            param_dict = {}
-        else:
-            self._update_standard_params(state, p, group, beta1, beta2)
+            if len(param_dict.keys()) == 2:
+                self._update_lora_params(state, param_dict, grad_dict, group, lora_scaler)
+                param_dict = {}
+                grad_dict = {}
+            else:
+                self._update_standard_params(state, param_fp32, grad, group, beta1, beta2)
+
 
     def _initialize_state(self, state, param_dict, p, group):
         state["step"] = torch.tensor(0.0, dtype=_get_scalar_dtype())
+        # Ensure optimizer states in torch.float32.
         if len(param_dict.keys()) == 2:
-            self._initialize_lora_state(state, param_dict, p.device, p.dtype, group["amsgrad"])
+            self._initialize_lora_state(state, param_dict, p.device, torch.float32, group["amsgrad"])
         else:
-            self._initialize_standard_state(state, p.shape, p.device, p.dtype, group["amsgrad"])
+            self._initialize_standard_state(state, p.shape, p.device, torch.flaot32, group["amsgrad"])
 
     def _initialize_lora_state(self, state, param_dict, device, dtype, amsgrad):
         state["exp_avg_A"] = torch.zeros(param_dict['weight_a'].shape).to(device).to(dtype)
@@ -210,34 +217,30 @@ class LoRAProAdamW(Optimizer):
         if amsgrad:
             state["max_exp_avg_sq"] = torch.zeros(shape).to(device).to(dtype)
     
-    def _update_lora_params(self, state, param_dict, group, scaling_factor):
+    def _update_lora_params(self, state, param_dict, grad_dict, group, lora_scaler):
         A = param_dict['weight_a']
         B = param_dict['weight_b']
-        grad_A_orin = A.grad
-        grad_B_orin = B.grad
-        
+        lora_rank, in_features = A.shape
+        out_features, _ = B.shape
+        grad_A_orin_fp32 = grad_dict['weight_a'].to(torch.float32)
+        grad_B_orin_fp32 = grad_dict['weight_b'].to(torch.float32)
+
         delta = 1e-8
-        AA_T = A @ A.T
-        B_TB = B.T @ B
-        AA_T_inv = torch.linalg.pinv(AA_T + delta * torch.eye(A.shape[0]).to(A.device)).to(A.dtype)
-        B_TB_inv = torch.linalg.pinv(B_TB + delta * torch.eye(A.shape[0]).to(A.device)).to(A.dtype)
+        AA_T = torch.matmul(A, A.T)
+        B_TB = torch.matmul(B.T, B)
+        AA_T_inv = torch.linalg.pinv(AA_T + delta * torch.eye(lora_rank).to(A.device)).to(A.dtype)
+        B_TB_inv = torch.linalg.pinv(B_TB + delta * torch.eye(lora_rank).to(A.device)).to(A.dtype)
 
-        X = self._compute_X(group, B, A, scaling_factor, grad_A_orin, grad_B_orin, B_TB_inv, AA_T, AA_T_inv).to(A.dtype)
+        X = self._compute_X(group, B, A, lora_scaler, grad_A_orin_fp32, grad_B_orin_fp32, B_TB_inv, AA_T, B_TB).to(B.device).to(B.dtype)
+
+        # [r,r], [r, d] -> [r, d]
+        B_TB_inv_B_T = torch.matmul(B_TB_inv, B.T)
+        I_minus_BBT_inv = torch.eye(out_features, device=B.device, dtype=B.dtype) - torch.matmul(B, B_TB_inv_B_T)
         
-        grad_A = (1 / scaling_factor ** 2) * B_TB_inv @ grad_A_orin + X @ A
-        grad_B = (1 / scaling_factor ** 2) * ((torch.eye(B.shape[0]).to(B.device).to(B.dtype) - B @ B_TB_inv @ B.T) @ grad_B_orin @ AA_T_inv) - B @ X
-    
-        self._adamw_update(state, group, A, B, grad_A, grad_B)
+        grad_scale = (1 / lora_scaler ** 2)
+        grad_A_fp32 = grad_scale * torch.matmul(B_TB_inv, grad_A_orin_fp32) + torch.matmul(X, A)
+        grad_B_fp32 = grad_scale * (torch.matmul(I_minus_BBT_inv, torch.matmul(grad_B_orin_fp32, AA_T_inv))) - torch.matmul(B, X)
 
-    def _compute_X(self, group, B, A, scaling_factor, grad_A_orin, grad_B_orin, B_TB_inv, AA_T, AA_T_inv):
-        if group['X_mode'] == "sylvester":
-            return solve_sylvester(B.T @ B, A @ A.T, -(1 / scaling_factor ** 2) * B_TB_inv @ grad_A_orin @ A.T)
-        elif group['X_mode'] == "symmetry":
-            return -0.5 * (1 / scaling_factor ** 2) * B_TB_inv @ B.T @ grad_B_orin @ AA_T
-        else:
-            return torch.zeros((B_TB_inv.shape[0], B_TB_inv.shape[0])).to(B.device)
-
-    def _adamw_update(self, state, group, A, B, grad_A, grad_B):
         exp_avg_A = state["exp_avg_A"]
         exp_avg_sq_A = state["exp_avg_sq_A"]
         
@@ -248,10 +251,10 @@ class LoRAProAdamW(Optimizer):
 
         step_t += 1
 
-        exp_avg_A.lerp_(grad_A, 1 - group["betas"][0])
-        exp_avg_B.lerp_(grad_B, 1 - group["betas"][0])
-        exp_avg_sq_A.mul_(group["betas"][1]).addcmul_(grad_A, grad_A.conj(), value=1 - group["betas"][1])
-        exp_avg_sq_B.mul_(group["betas"][1]).addcmul_(grad_B, grad_B.conj(), value=1 - group["betas"][1])
+        exp_avg_A.lerp_(grad_A_fp32, 1 - group["betas"][0])
+        exp_avg_B.lerp_(grad_B_fp32, 1 - group["betas"][0])
+        exp_avg_sq_A.mul_(group["betas"][1]).addcmul_(grad_A_fp32, grad_A_fp32.conj(), value=1 - group["betas"][1])
+        exp_avg_sq_B.mul_(group["betas"][1]).addcmul_(grad_B_fp32, grad_B_fp32.conj(), value=1 - group["betas"][1])
 
         step = _get_value(step_t)
         
@@ -280,16 +283,24 @@ class LoRAProAdamW(Optimizer):
         A.addcdiv_(exp_avg_A / bias_correction1, denom_A, value=-step_size)
         B.addcdiv_(exp_avg_B / bias_correction1, denom_B, value=-step_size_b)
 
-    def _update_standard_params(self, state, p, group, beta1, beta2):
-        grad = p.grad
+    def _compute_X(self, group, B, A, lora_scaler, grad_A_orin_fp32, grad_B_orin_fp32, B_TB_inv, AA_T, B_TB):
+        if group['X_mode'] == "sylvester":
+            return solve_sylvester(B_TB, AA_T, -(1 / lora_scaler ** 2) * torch.matmul(torch.matmul(B_TB_inv, grad_A_orin_fp32), A.T))
+        elif group['X_mode'] == "symmetry":
+            return -0.5 * (1 / lora_scaler ** 2) * torch.matmul(torch.matmul(B_TB_inv, B.T), torch.matmul(grad_B_orin_fp32, AA_T))
+        else:
+            return torch.zeros((B_TB_inv.shape[0], B_TB_inv.shape[0]))
+
+    def _update_standard_params(self, state, param, grad, group, beta1, beta2):
         exp_avg = state["exp_avg"]
         exp_avg_sq = state["exp_avg_sq"]
         step_t = state["step"]
 
         step_t += 1
 
-        exp_avg.lerp_(grad, 1 - beta1)
-        exp_avg_sq.mul_(beta2).addcmul_(grad, grad.conj(), value=1 - beta2)
+        grad_fp32 = grad.float()
+        exp_avg.lerp_(grad_fp32, 1 - beta1)
+        exp_avg_sq.mul_(beta2).addcmul_(grad_fp32, grad_fp32.conj(), value=1 - beta2)
 
         step = _get_value(step_t)
 
@@ -301,6 +312,6 @@ class LoRAProAdamW(Optimizer):
         bias_correction2_sqrt = _dispatch_sqrt(bias_correction2)
         denom = (exp_avg_sq.sqrt() / bias_correction2_sqrt).add_(group['eps'])
         if group['weight_decay'] != 0:
-            p.mul_(1 - group["weight_decay"] * group["lr"])
+            param.mul_(1 - group["weight_decay"] * group["lr"])
         
-        p.addcdiv_(exp_avg / bias_correction1, denom, value=-step_size)
+        param.addcdiv_(exp_avg / bias_correction1, denom, value=-step_size)
