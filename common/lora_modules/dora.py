abd36528181aa70fb3c2515c2a93bb3f87643401
hhnqqq <895228612@qq.com>
2024-11-13 17:09:06 +0800

    优化dora代码

--- common/lora_modules/dora.py
+++ common/lora_modules/dora.py
@@ -1,3 +1,21 @@
+"""
+Implementation of DoRA
+
+Below is the specific method from the dora paper:
+This analysis examines the updates in
+both magnitude and direction of the LoRA and FT weights
+relative to the pre-trained weights to reveal the fundamental
+differences in the learning behaviors of both. The weight
+decomposition of W ∈ Rdxk can be formulated as:
+ W =m x V / ||V ||c = ||W||c x W / ||W||c
+
+where m ∈ R1xk is the magnitude vector, V ∈ Rdxk is the
+directional matrix, with ||·||c being the vector-wise norm of
+a matrix across each column. This decomposition ensures
+that each column of V/||V ||c remains a unit vector, and the
+corresponding scalar in m defines the magnitude of each
+vector.
+"""
 from common.lora_modules.lora import *
 
 class LinearWithDoRA(LinearWithLoRA):
@@ -7,6 +25,9 @@ class LinearWithDoRA(LinearWithLoRA):
         if lora_config.lora_dropout:
             print(f'Dora is incompatible with lora dropout, skiped lora dropout')
 
+    def _init_lora_weights(self):
+        super()._init_lora_weights()
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # The origin weight of Linear layer.
         weight = self._quantize_weight(self.weight, self.weight_quantizer)
@@ -14,13 +35,16 @@ class LinearWithDoRA(LinearWithLoRA):
         return F.linear(x, weight)
     
     def _apply_dora(self, weight: torch.Tensor) -> torch.Tensor:
-        origin_magnitude = torch.norm(weight, p=2, dim=0, keepdim=True)
-        lora_weight = self._compute_lora_weight()
         # Make sure that the dtype of weight same as dtype of lora weights.
+        lora_weight = self._compute_lora_weight()
+
         origin_weight_dtype = weight.dtype
+        # Compute column-wise L2 norm.
+        origin_magnitude: torch.Tensor = torch.linalg.norm(weight.detach(), dim=1).to(lora_weight.dtype)
+        
         weight = weight.to(lora_weight.dtype)
         weight = weight + lora_weight * self.lora_scaler
-        new_magnitude = torch.norm(weight, p=2, dim=0, keepdim=True)
+        new_magnitude: torch.Tensor = torch.linalg.norm(weight.detach(), dim=1).to(lora_weight.dtype)
         # see section 4.3 of DoRA (https://arxiv.org/abs/2402.09353)
         # "[...] we suggest treating ||V +∆V ||_c in
         # Eq. (5) as a constant, thereby detaching it from the gradient
@@ -28,12 +52,14 @@ class LinearWithDoRA(LinearWithLoRA):
         # reflects the updates of ∆V , it won’t receive any gradient
         # during backpropagation"
         new_magnitude = new_magnitude.detach()
+        origin_magnitude = origin_magnitude.detach()
 
         # In peft. This should be added on top of the base layer output.
         # result_dora = (mag_norm_scale - 1) * (
         # F.linear(x, transpose(weight, self.fan_in_fan_out))
         # ) + mag_norm_scale * lora_result * scaling
-        weight = (origin_magnitude / new_magnitude) * weight
+        mag_norm_scale = (origin_magnitude / new_magnitude).view(-1, 1)
+        weight = mag_norm_scale * weight
         return weight.to(origin_weight_dtype)
 
     def _merge_lora(self) -> bool:
